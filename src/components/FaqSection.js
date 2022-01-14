95316560c8479a0d1ebaadf64c8276f7ea47c0bc
Quentin Benard Diarra <quentin.benarddiarra@gmail.com>
2022-01-15 00:45:18 +0100

    4. Faq Section setup

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div>
+      <div className="faq">
+        <h2>Any questions ?</h2>
+        <div className="question">
+          <h4>How Do I Start ?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab,
+              iste!
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily Schedule ?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab,
+              iste!
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>Differement Payment Methods</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab,
+              iste!
+            </p>
+          </div>
+        </div>
+        <div className="question">
+          <h4>What Products Do You Offer ?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab,
+              iste!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
