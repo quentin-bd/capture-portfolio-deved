90a81c346727252445773224a07f71bfc38eb4dd
Quentin Benard Diarra <quentin.benarddiarra@gmail.com>
2022-01-15 01:46:01 +0100

    5. Styled Component basics

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,70 @@
-import React from 'react'
-import home1 from "../img/home1.png"
+import React from "react";
+// image
+import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
 
 function AboutSection() {
-    return (
-        <div>
-            <div>
-                <div className="description">
-                    <div className="title">
-                        <div className="hide">
-                            <h2>We work to make</h2>
-                        </div>
-                        <div className="hide">
-                            <h2>Your <span>dreams</span> come</h2>
-                        </div>
-                        <div className="hide">
-                            <h2>true.</h2>
-                        </div>
-                    </div>
-                    <p>Contact us for any photgraphy or videographys ideas that you have. We have professionnals with amazing skills.</p>
-                    <button>Contact us</button>
-                </div>
-                <div className="image">
-                    <img src={home1} alt="camera dude"/>
-                </div>
-            </div>
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              Your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
         </div>
-    )
+        <p>
+          Contact us for any photgraphy or videographys ideas that you have. We
+          have professionnals with amazing skills.
+        </p>
+        <button>Contact us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="camera dude" />
+      </Image>
+    </About>
+  );
 }
 
-export default AboutSection
+// Styled Components
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`
+
+export default AboutSection;
