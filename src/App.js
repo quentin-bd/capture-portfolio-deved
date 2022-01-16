0506bc783fed1d2b09855e8968065db5c0bd2c95
Quentin Benard Diarra <quentin.benarddiarra@gmail.com>
2022-01-16 23:25:17 +0100

    10. Our Work basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+// Components
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
+// Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<AboutUs />}></Route>
+        <Route path="/work" element={<OurWork />}></Route>
+        <Route path="/contact" element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
