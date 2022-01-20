d85daf222490053ab886dcd45eb32d1cfda0bde8
Quentin Benard Diarra <quentin.benarddiarra@gmail.com>
2022-01-20 15:45:43 +0100

    Wave animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,8 +2,10 @@ import React from "react";
 // image
 import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
-// Framer Motion
+// Framer Motion & animations
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from './Wave'
 
 function AboutSection() {
   return (
@@ -11,26 +13,27 @@ function AboutSection() {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               Your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photgraphy or videographys ideas that you have. We
           have professionnals with amazing skills.
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="camera dude" />
+        <motion.img variants={photoAnim} src={home1} alt="camera dude" />
       </Image>
+      <Wave />
     </About>
   );
 }
